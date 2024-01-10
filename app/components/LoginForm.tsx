bbf4d4eb1fa47184577a9ba1f94831a49f37bb72
aliraza-carbonteq <aliraza.khalid@carbonteq.com>
2024-01-10 22:35:42 +0500

    added login card ui

---
+++ app/components/LoginForm.tsx
@@ -0,0 +1,10 @@
+import { Button, Card } from 'antd'
+import React from 'react'
+
+export default function LoginForm() {
+  return (
+    <Card title="Login" style={{ width: 300 }}>
+        <Button value="large" block type='primary'>Log in using Square</Button>
+    </Card>
+  )
+}
