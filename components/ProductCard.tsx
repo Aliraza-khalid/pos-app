636ac633c5d2281a97ff2506b3d06803d5159584
aliraza-carbonteq <aliraza.khalid@carbonteq.com>
2024-01-19 15:32:10 +0500

    added useProductCart hook

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,73 +1,19 @@
 "use client";
 
-import { CartProduct } from "@/types/Cart";
 import { CatalogProduct } from "@/types/Product";
 import FormatPrice from "@/utils/formatPrice";
+import useProductCart from "@/utils/hooks/useProductCart";
 import { Button, Card, Flex, Typography } from "antd";
 import { createStyles } from "antd-style";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 type PropTypes = {
   item: CatalogProduct;
-};
+}
 
 export default function ProductCard({ item }: PropTypes) {
-  const [quantity, setQuantity] = useState(0);
   const { styles, cx, theme } = useStyles();
-
-  useEffect(() => {
-    const cart = localStorage.getItem("cart");
-    if (!cart) return;
-
-    const product = JSON.parse(cart).find(
-      (p: CartProduct) => p.catalogObjectId === item.catalogObjectId
-    );
-    if (!product) return;
-
-    setQuantity(product.quantity);
-  }, [item.catalogObjectId]);
-
-  const onClickAdd = () => {
-    setQuantity((q) => q + 1);
-    updateCart({
-      ...item,
-      variation: item.variations[0],
-      quantity: quantity + 1,
-    });
-  };
-
-  const onClickSubstract = () => {
-    setQuantity((q) => q - 1);
-    quantity === 1
-      ? removeFromCart(item.catalogObjectId)
-      : updateCart({
-          ...item,
-          variation: item.variations[0],
-          quantity: quantity - 1,
-        });
-  };
-
-  const updateCart = (product: CartProduct) => {
-    const cart = localStorage.getItem("cart");
-    if (!cart) return localStorage.setItem("cart", JSON.stringify([product]));
-
-    localStorage.setItem(
-      "cart",
-      JSON.stringify([
-        ...JSON.parse(cart).filter((p: CartProduct) => p.catalogObjectId !== product.catalogObjectId),
-        product,
-      ])
-    );
-  };
-
-  const removeFromCart = (id: string) => {
-    const cart = localStorage.getItem("cart");
-    if (cart)
-      return localStorage.setItem(
-        "cart",
-        JSON.stringify(JSON.parse(cart).filter((p: CartProduct) => p.catalogObjectId !== id))
-      );
-  };
+  const {quantity, onClickAdd, onClickSubstract} = useProductCart({item});
 
   return (
     <Card
