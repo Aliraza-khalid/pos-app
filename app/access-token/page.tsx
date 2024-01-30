bc40bf4b7f94643b6a92ef1fdc44dd6368a04142
aliraza-carbonteq <aliraza.khalid@carbonteq.com>
2024-01-30 20:50:04 +0500

    minor changes

--- app/access-token/page.tsx
+++ app/access-token/page.tsx
@@ -1,35 +1,35 @@
 "use client";
 
-import LoginFailedCard from "@/components/auth/LoginFailedCard";
-import { PageProps } from "@/types/PageProps";
-import getAccessToken from "@/utils/apis/getAccessToken";
+import React, { useEffect, useRef, useState } from "react";
 import { Flex, Spin } from "antd";
 import { useRouter } from "next/navigation";
-import React, { useEffect, useRef, useState } from "react";
+import LoginFailedCard from "@/components/auth/LoginFailedCard";
+import getAccessToken from "@/utils/apis/getAccessToken";
+import { PageProps } from "@/types/PageProps";
 
-export default function AccessToken({searchParams}: PageProps) {
+export default function AccessToken({ searchParams }: PageProps) {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
   const code = searchParams?.code;
   const apiCalled = useRef(false);
 
   useEffect(() => {
-    if(code && !apiCalled.current) {
+    const verifyToken = async (code: string) => {
+      setLoading(true);
+      const result = await getAccessToken(code);
+      if (result) {
+        localStorage.setItem("accessToken", JSON.stringify(result.token));
+        localStorage.setItem("merchant", JSON.stringify(result.merchant));
+        localStorage.setItem("locations", JSON.stringify(result.locations));
+        router.replace("/dashboard");
+      } else setLoading(false);
+    };
+
+    if (code && !apiCalled.current) {
       verifyToken(code as string);
       apiCalled.current = true;
     }
-  }, [code]);
-
-  const verifyToken = async (code: string) => {
-    setLoading(true);
-    const result = await getAccessToken(code);
-    if (result) {
-      localStorage.setItem("accessToken", JSON.stringify(result.token));
-      localStorage.setItem("merchant", JSON.stringify(result.merchant));
-      localStorage.setItem("locations", JSON.stringify(result.locations));
-      router.replace("/dashboard");
-    } else setLoading(false);
-  };
+  }, [code, router]);
 
   return (
     <Flex justify="center" align="center" style={{ height: "100vh" }}>
